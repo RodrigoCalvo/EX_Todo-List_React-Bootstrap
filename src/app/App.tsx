cfb5fc14c17987d39d852bc7848b4fe5ff67627f
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-08-13 16:16:35 +0200

    Add routes export

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from '../components/layout';
 
+const HomePage = React.lazy(() => import('../pages/home'));
+const TodoPage = React.lazy(() => import('../pages/todo'));
+const AboutPage = React.lazy(() => import('../pages/about'));
+export const routes = [
+  { path: '', label: 'Home', page: <HomePage></HomePage> },
+  { path: '/todo', label: 'ToDo', page: <TodoPage></TodoPage> },
+  { path: '/about', label: 'About', page: <AboutPage></AboutPage> },
+  { path: '*', label: '', page: <Navigate replace to=""></Navigate> },
+];
+
 function App() {
-  const HomePage = React.lazy(() => import('../pages/home'));
-  const TodoPage = React.lazy(() => import('../pages/todo'));
-  const AboutPage = React.lazy(() => import('../pages/about'));
-  const routes = [
-    { path: '', label: 'Home', page: <HomePage></HomePage> },
-    { path: '/todo', label: 'Home', page: <TodoPage></TodoPage> },
-    { path: '/about', label: 'Home', page: <AboutPage></AboutPage> },
-    { path: '*', label: '', page: <Navigate replace to=""></Navigate> },
-  ];
-  const menuOptions = [...routes];
-  menuOptions.length = routes.length - 1; //eliminar default
   return (
     <Layout>
       <React.Suspense>
