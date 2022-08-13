ae6c6409e526f951af6213ebcc17b568c57d4f0f
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-08-13 19:07:15 +0200

    Add load from storage to redux store

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from '../components/layout';
+import { loadTasksAction } from '../redux/tasks/task.action.creators';
+import { getTasks } from '../services/tasks.local-storage.service';
 
 const HomePage = React.lazy(() => import('../pages/home'));
 const TodoPage = React.lazy(() => import('../pages/todo'));
@@ -13,6 +16,9 @@ export const routes = [
 ];
 
 function App() {
+  const tasks = getTasks();
+  const dispatcher = useDispatch();
+  dispatcher(loadTasksAction(tasks));
   return (
     <Layout>
       <React.Suspense>
