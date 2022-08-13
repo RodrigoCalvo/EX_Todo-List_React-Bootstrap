bfab0b4d918580a307490fb1eb658e7c248cdf3c
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-08-14 01:27:20 +0200

    Add bootstrap navbar style

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,16 +1,22 @@
+import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { routes } from '../app/App';
 
 export function Menu() {
   const menuOptions = routes.slice(0, routes.length - 1);
   const template = (
-    <ul>
-      {menuOptions.map((item) => (
-        <li key={item.label}>
-          <Link to={item.path}>{item.label}</Link>
-        </li>
-      ))}
-    </ul>
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Brand as={Link} to={'/'} className="ps-3">
+        To-Do
+      </Navbar.Brand>
+      <Nav>
+        {menuOptions.map((item) => (
+          <Nav.Link as={Link} key={item.label} to={item.path}>
+            {item.label}
+          </Nav.Link>
+        ))}
+      </Nav>
+    </Navbar>
   );
   return template;
 }
