13a9615100c9c72652da29bf09a1c0cc47b0ee5f
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-08-14 01:24:35 +0200

    Add footer styles: width, italic & center

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
 export function Footer() {
   const template = (
-    <footer>
-      <address>Rodrigo Calvo</address>
+    <footer
+      style={{ maxWidth: '50rem', margin: 'auto' }}
+      className="fixed-bottom text-center"
+    >
+      <address className="fst-italic">Rodrigo Calvo</address>
     </footer>
   );
   return template;
