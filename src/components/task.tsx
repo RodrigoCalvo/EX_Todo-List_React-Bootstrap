851f8903b86b33fabc5e49d032501384690531e7
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-08-14 01:29:07 +0200

    Add bootstrap Form styles

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { Button, Form, InputGroup } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { iTask } from '../models/task';
 import {
@@ -10,6 +12,7 @@ import {
 } from '../services/tasks.local-storage.service';
 
 export function Task({ data }: { data: iTask }) {
+  const [editable, setEditable] = useState(false);
   const dispatcher = useDispatch();
 
   function deleteTask() {
@@ -22,24 +25,41 @@ export function Task({ data }: { data: iTask }) {
     editTask(newTask);
   }
 
+  function toggleEdit() {
+    setEditable(!editable);
+  }
+
+  function editData() {
+    console.log('Guardando...');
+    toggleEdit();
+  }
+
   function editTask(editedTask: iTask) {
-    console.log(updateTask(editedTask));
+    updateTask(editedTask);
     dispatcher(updateTaskAction(editedTask));
   }
 
   const template = (
-    <p>
-      <span>{data.responsible}</span>
-      <span>{data.description}</span>
-      <input
-        type="checkbox"
-        name="completed"
-        id="completed"
-        checked={data.completed}
-        onChange={toggleCompleted}
-      />
-      <input type="button" value="Delete" onClick={deleteTask} />
-    </p>
+    <>
+      <InputGroup>
+        <InputGroup.Checkbox
+          checked={data.completed}
+          onChange={toggleCompleted}
+        />
+        <Form.Control
+          value={data.description}
+          disabled={!editable}
+        ></Form.Control>
+        <Form.Control
+          value={data.responsible}
+          disabled={!editable}
+        ></Form.Control>
+        <Button onClick={!editable ? toggleEdit : editData}>
+          {editable ? 'Save' : 'Edit'}
+        </Button>
+        <Button onClick={deleteTask}>Delete</Button>
+      </InputGroup>
+    </>
   );
   return template;
 }
