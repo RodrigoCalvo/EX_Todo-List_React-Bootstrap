1959e32eb2f7ffc4f8f51714d8c6516b92bcee72
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-08-13 19:10:00 +0200

    Add about page component

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import { Contact } from '../components/contact';
+
+export function About() {
+  const template = (
+    <>
+      <h2>Contact</h2>
+      <Contact></Contact>
+    </>
+  );
+  return template;
+}
+
+export default About;
